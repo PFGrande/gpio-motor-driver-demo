aea4476c815a942d120988d4f58d0775634ce9ed
Pedro <2002.pedrogrande@gmail.com>
2024-04-17 14:23:52 +0100

    added ociltor code

--- Channels.h
+++ Channels.h
@@ -7,5 +7,10 @@
 #define LED0_OFF_L  0x08
 #define LED0_OFF_H  0x09
 
+#define MODE_REGISTER_1 0x00
+
+/* "The PRE_SCALE register can only be set
+ when the SLEEP bit of MODE1 register is set to logic 1." 
+ (PCA96_datasheet, 24)*/
 // the value used to determine the circuit's frequency
 #define PRE_SCALE_REGISTER 0xFE
--- MotorDriver.c
+++ MotorDriver.c
@@ -14,6 +14,8 @@ void terminateMotorDriver() {
 // return 0 on success
 // valid frequencies: 0-4095 (not as an input but as an actual value)
 int setPWMfreq(int frequency) {
+    i2cWriteByteData(); // dissable the ocilator (set it to sleep mode) to 
+    // change the pre_scale register (the ocilator's frequency)
     // currently don't use frequency parameter, use testing variable
     // return i2cWriteByteData(motorDriverHandle, );
 }
